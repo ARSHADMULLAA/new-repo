ebbaa92de5ec2cb063e0d18ca5877840f77a6019
ARSHADMULLA <khairulmatin302@gmail.com>
2025-04-24 11:29:47 +0530

    demo.java

---
+++ demo.java
@@ -0,0 +1,8 @@
+pubic class add two number {
+public static void main (string[] args) {
+    int num 1 = 10;
+    int num 2 = 20;
+    int sum = num1+num2;
+    system.out.printin("sumo of two numbers" "+ num1+ and num2+" is" +sum")
+}
+}
